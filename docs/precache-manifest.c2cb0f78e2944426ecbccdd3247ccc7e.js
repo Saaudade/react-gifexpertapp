1e7b8b0792a65fc7f0b99694539f37848e3eb338
Christian Tamayo <giby098@hotmail.com>
2020-09-14 15:21:49 -0500

    docs agregado

---
+++ docs/precache-manifest.c2cb0f78e2944426ecbccdd3247ccc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36e1ac9273c10271329801dc4ea08ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1200502f32548c62233",
+    "url": "/static/css/main.3687fc72.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1200502f32548c62233",
+    "url": "/static/js/main.4e5f4d0a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
